96f34f35710208b024ac1b7502528c0a72beb073
shubhi3199 <shbhm3199@gmail.com>
2019-08-24 11:10:56 +0530

    Highlighting Error field patch.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,23 +3,33 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component{
 
-    renderInput({input, label, meta}){
-
+    renderError({touched, error}){            //destructuring touched and error properties from meta object
+        if(touched && error){
+            return(
+                <div className={`ui error message`}>
+                    <div className={`header`}>{error}</div>
+                </div>
+            );
+        }
+    }
+    renderInput=({input, label, meta})=>{
+        const className=`field ${meta.error && meta.touched ? 'error':''}`;
         return(
-            <div className={`field`}>
+            <div className={className}>
                 <label>{label}</label>
-                <input {...input}/>
-                <div>{meta.error}</div>
-            </div>
-
-                )}
+                <input {...input}  autoComplete={`off`}/>
+                {this.renderError(meta)}
+            </div>                                  //here as renderInput() is getting passed to some other
+        )};                                         //component the value of this inside renderInput() will
+                                                    //be undefined.so to prevent error we gotta bind renderInput()
+                                                    //using an arrow function.
 
     onSubmit(formValues){
         console.log(formValues);
     }
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className={`ui form`}>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className={`ui form error`}>
                 <Field name={`title`} component={this.renderInput} label={`Enter Title`} />
                 <Field name={`description`} component={this.renderInput} label={`Enter Description`} />
                 <button className={`ui button primary`}>Submit</button>
