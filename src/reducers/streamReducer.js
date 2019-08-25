70f02bc23dca1d623f6a4870e8969a8e04ed149f
shubhi3199 <shbhm3199@gmail.com>
2019-08-26 00:46:42 +0530

    Rendering all Streams on to the main Webpage.

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -10,7 +10,7 @@ import {
 export default (state={}, action)=>{
     switch (action.type) {
         case FETCH_STREAMS:
-            return {...state, ..._.mapKeys(action.payload, 'id')};
+            return {...state, ..._.mapKeys(action.payload,'id')};
         case FETCH_STREAM:
             return {...state, [action.payload.id]: action.payload};
         case CREATE_STREAM:
