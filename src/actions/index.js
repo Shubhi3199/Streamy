d6a2440b91131241a5524945df4a2f4d0dd6f2e0
shubhi3199 <shbhm3199@gmail.com>
2019-08-25 13:25:46 +0530

    Creating all Action Creators.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM} from "./types";
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from "./types";
 
 export const signIn=(userId)=>{                  //actionCreator()
     return{
@@ -14,10 +22,32 @@ export const signOut=()=>{                       //actionCreator()
 };
 export const createStream=(formValues)=>{        //actionCreator()
     return async ( dispatch)=>{
-       const response=await streams.post('/streams', formValues)  //creating a handle for the newly created stream
+       const response=await streams.post('/streams', formValues); //creating a handle for the newly created stream
                                                                   //so that we can handle the action dispatched.
     dispatch({type:CREATE_STREAM, payload: response.data});       //dispatching the action object with the payload
     };                                                           //recived from axios response.
 
 };
+export const fetchStreams=()=> async dispatch=>{
+    const response=await streams.get('/streams');
+    dispatch({type:FETCH_STREAMS, payload:response.data});
+};
+
+export const fetchStream=(id)=>async dispatch=>{
+    const response=await streams.get(`/streams/${id}`);
+    dispatch({type:FETCH_STREAM, payload:response.data});
+};
+
+export const editStream=(id, formValues)=>async dispatch=>{
+    const response=await streams.put(`/streams/${id}`, formValues);
+    dispatch({type:EDIT_STREAM, payload:response.data});
+};
+
+export const deleteStream=(id)=>async dispatch=>{
+    const response=await streams.delete(`streams/${id}`);
+    dispatch({type:DELETE_STREAM, payload:id});
+};
+
+
+
 
