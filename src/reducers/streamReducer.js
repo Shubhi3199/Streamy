85f23fae5bfde59d3dfef9bd0f6412663d3bc63a
shubhi3199 <shbhm3199@gmail.com>
2019-08-25 21:52:13 +0530

    1. Creating streamReducer. 2. Handling fetching, updating, creating ,  and deleting exisiting records from the streamReducer using lodash library. 3. Merging lists of records using _.mapKeys() from lodash.

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  CREATE_STREAM
+} from "../actions/types";
+
+export default (state={}, action)=>{
+    switch (action.type) {
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload, 'id')};
+        case FETCH_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case CREATE_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case EDIT_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);  //omit does not changes the initial state but actually creates a new
+                                                   //object .
+        default:
+            return state;
+    }
+};
+
