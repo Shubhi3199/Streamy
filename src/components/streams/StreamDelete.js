d786765d299ef66fb42fa9db85147e990d93d69d
shubhi3199 <shbhm3199@gmail.com>
2019-08-29 11:00:46 +0530

    Using React Portals to make a Reusable Modal Window.

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete=()=>{
-    return <div>StreamDelete</div>;
+    const actions=(
+       <div>
+           <button className={`ui button negative`}>Delete</button>
+            <button className={`ui button`}>Cancel</button>
+       </div>
+    );
+    return(
+        <div>
+            StreamDelete
+            <Modal
+                title={`Delete Stream`}
+                content={`Are you sure, you want to Delete this stream?`}
+                onDismiss={()=>history.push('/')}
+                actions={actions}
+
+            />
+        </div>
+    );
 };
 
 export default StreamDelete;
+
